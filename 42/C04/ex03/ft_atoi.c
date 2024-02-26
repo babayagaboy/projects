bccfe825776a4966c8b1a9f1f82996989aece4c9
Hugo Guterres <hugo.guterres.2005@gmail.com>
2024-02-26 11:53:04 +0000

    b

--- 42/C04/ex03/ft_atoi.c
+++ 42/C04/ex03/ft_atoi.c
@@ -10,25 +10,35 @@ int ft_atoi(char *str)
     result = 0;
     isneg = 1;
     i = 0;
-    while (str[i] >= 9 && str[i] <= 13 || str[i] == ' ')
+
+    while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
         i++;
+
     while (str[i] == '+' || str[i] == '-')
     {
         if (str[i] == '-')
             isneg *= -1;
         i++;
     }
+
     while (str[i] >= 0 && str[i] <= 9)
     {
-        result *= 10;
-        result = str[i] + 0;
+        result = result * 10 + (str[i] - '0');
+        i++;
     }
-    return(result);
+
+    return(result * isneg);
 }
 
 int main(int argc, char **argv)
 {
-    printf("%d", ft_atoi("12"));
-    //printf("this is the number you wanted: %d\n" , ft_atoi(argv[1]));
+    printf("%d\n", ft_atoi("123"));
+    printf("%d\n", ft_atoi("-123"));
+    printf("%d\n", ft_atoi("123asd"));
+
+    // if (argc >= 1)
+    //     printf("this is the number you wanted: %d\n" , ft_atoi(argv[1]));
+    // else
+    //     printf("erro");
     return (0);
 }

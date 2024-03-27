140a7da688c2548b67e632ad425ad32f4fb5aacb
Hugo Guterres <hugo.guterres.2005@gmail.com>
2024-03-27 10:21:04 +0000

    a

--- IEFP/Python/Python Avancado/ex00/a.py
+++ IEFP/Python/Python Avancado/ex00/a.py
@@ -7,6 +7,9 @@ def update_time():
     label2.config(text= current_time)
     window.after(1000, update_time)
 
+def close_window():
+    window.destroy()
+
 window = tk.Tk()
 window.title("test")
 
@@ -17,7 +20,7 @@ label2 = tk.Label(window, text= '')
 label2.pack(padx= 50, pady= 50)
 update_time()
 
-close_button = tk.Button(window, text= "CLOSE")
+close_button = tk.Button(window, text= "CLOSE" , command= close_window())
 close_button.pack(padx= 25, pady= 25)
 
 window.mainloop()
